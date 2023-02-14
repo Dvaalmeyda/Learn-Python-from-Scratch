a53572289eb5871cc3cfec058eb7f2272e179e25
Diva Putra Almeyda <122163249+Dvaalmeyda@users.noreply.github.com>
2023-02-14 15:26:18 +0700

    Add files via upload

---
+++ 3. Expression & Variables.py
@@ -0,0 +1,20 @@
+#Basic Arithmetic Operation
+10 + 10 #additon
+10 - 10 #subtraction
+10 * 10 #multipiclation
+10 / 10 #division
+6 // 5 #integer division
+
+#variables
+
+x = 243 + 354
+print(x)
+
+y = x * 5
+print(y)
+
+a = 234 + 10 * 5 #multiplication first
+print(a)
+
+b = (234 + 10) * 5 #addition first
+print(b)
