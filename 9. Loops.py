23ba0ee66a621716dfbb2b7d3a31c5ee4a31e986
Diva Putra Almeyda <122163249+Dvaalmeyda@users.noreply.github.com>
2023-02-21 15:28:22 +0700

    Update 9. Loops.py

--- 9. Loops.py
+++ 9. Loops.py
@@ -1,4 +1,23 @@
-A=[3,4,5]
+#FOR LOOPS
 
-for a in A:
-    print(a)
+#squares=["red","green","Blue"]
+#print(squares)
+#for i in range(0,3):
+#    squares[i]="white"
+#print(squares)
+
+#or
+
+#for i in squares:
+#    squares[i]="white"
+#    print(i)
+    
+#WHILE LOOPS
+
+PlayListRatings = [10, 9.5, 10, 8, 7.5, 5, 10, 10]
+i = 0
+rating = PlayListRatings[0]
+while(i < len(PlayListRatings) and rating >= 6):
+    print(rating)
+    i = i + 1 
+    rating = PlayListRatings[i]   
